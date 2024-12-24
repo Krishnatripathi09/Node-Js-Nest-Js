dfba1462b776716bc4d50762884732a73739c5f3
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-24 16:27:49 +0530

    docs:studied how to export and import multiple things at once in NodeJS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-require("./xyz");
+require("./xyz.js");
 
-const calculateS = require("./xyz");
+const obj = require("./xyz.js");
 var name = "Krishna Tripathi";
 console.log(name);
-calculateS(1, 2);
+obj.calculateS(1, 2);
+obj.x;
+console.log(obj.x);
 //by default to import anything in nodejs we need to write require and filename from where we are importing
 //we can return  what function the require module is exporting in a variable and use it later as required.
--- xyz.js
+++ xyz.js
@@ -1,6 +1,12 @@
 function calculateS(a, b) {
   console.log(a + b);
 }
-module.exports = calculateS;
-// by default node js uses commonJS modules so TO export any thing we have to use module.export = 
-// function which we want to export  
+var x = 19;
+module.exports = {
+  calculateS: calculateS,
+  x: x,
+};
+
+//To export multiple things in nodeJs we have to wrap it in a Object as key value pair as above
+// by default node js uses commonJS modules so TO export any thing we have to use module.export =
+// function which we want to export
