8ab8c3bca957e29eca7a8a2cef3dcc1dbc027e16
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-24 16:19:29 +0530

    docs: Studied import and export modules in Node.js, including CommonJS and ES Modules

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require("./xyz");
+
+const calculateS = require("./xyz");
 var name = "Krishna Tripathi";
 console.log(name);
-var a = 10;
-var b = 10;
-console.log(a + b);
+calculateS(1, 2);
+//by default to import anything in nodejs we need to write require and filename from where we are importing
+//we can return  what function the require module is exporting in a variable and use it later as required.
---
+++ xyz.js
@@ -0,0 +1,6 @@
+function calculateS(a, b) {
+  console.log(a + b);
+}
+module.exports = calculateS;
+// by default node js uses commonJS modules so TO export any thing we have to use module.export = 
+// function which we want to export  
