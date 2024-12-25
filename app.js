c8249c54d57f4733af4da66a674af411650ff5ff
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-25 14:28:44 +0530

    docs: Learned how to import and use data from a JSON file in Node.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let {  multiplyNum, calculateSum } = require("./calculate/index");
+let { multiplyNum, calculateSum } = require("./calculate/index");
+const data = require("./data.json");
+console.log(data);
 var name = "Krishna Tripathi";
+
 console.log(name);
 calculateSum(1, 2);
 multiplyNum(2, 10);
-
