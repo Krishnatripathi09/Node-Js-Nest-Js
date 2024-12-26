c9d69c95445be619cbbf76eb22be4179c222fd10
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-26 18:43:13 +0530

    feat: explore blocking execution behavior in Node.js

---
+++ blocking.js
@@ -0,0 +1,24 @@
+const crypto = require("node:crypto");
+
+console.log("Hi World");
+
+var a = 10893678;
+var b = 45678789;
+
+crypto.pbkdf2Sync("password", "salt", 5000000, 50, "sha512");
+console.log("first key is Generated");
+
+//crypto.pbkdf2 //password based key derivative function
+// Async Function
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+  console.log("second Key is generated");
+});
+
+function multiplyFn(x, y) {
+  const result = a * b;
+  return result;
+}
+
+var c = multiplyFn(a, b);
+
+console.log("Multiplication result is : ", c);
