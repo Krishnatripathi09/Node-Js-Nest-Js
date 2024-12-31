7e9b82b9894ee5ee48173d26f48e94469eceff37
Krishna Tripathi <192.168.9.99kj@gmail.com>
2024-12-31 14:52:32 +0530

    feat: integrate MongoDB connection string and install MongoDB package

---
+++ database.js
@@ -0,0 +1,2 @@
+const URI =
+  "mongodb+srv://react001:ij3gIfWaMNhCQIcz@cluster0.bhawl.mongodb.net/";
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const http = require("http");
 const server = http.createServer(function (req, res) {
   //reply with hello world
 
+  if (req.url === "/getSecretData") {
+    res.end("Chala Ja Bhosiya: No Secret Data :)");
+  }
   res.end("Hello World!");
 });
 
